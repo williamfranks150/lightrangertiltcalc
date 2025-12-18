ff9d8a5c3000f39ffc4806af96a5612ffa8805db
williamfranks150 <william.franks@gmail.com>
2025-12-18 09:54:50 -0500

    Refactor service worker for cache management
    Updated service worker to use a new cache version and modified asset handling.

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
-const CACHE = "lr-tilt-v200";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
+const CACHE = "lr-tilt-v1";
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then((c) => c.add("./")));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
 self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request).then((r) => {
+      const copy = r.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match(e.request).then((r) => r || caches.match("./")))
+  );
 });
