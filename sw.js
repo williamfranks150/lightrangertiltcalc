1a1ff3c4f0e210797243a7372308c302945162f4
williamfranks150 <william.franks@gmail.com>
2025-12-18 10:18:43 -0500

    Update service worker to new cache version and logic

--- sw.js
+++ sw.js
@@ -1,16 +1,25 @@
-const CACHE = "lr-tilt-v1";
+const CACHE = "lr-tilt-v300";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js"
+];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.add("./")));
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    fetch(e.request).then((r) => {
-      const copy = r.clone();
-      caches.open(CACHE).then((c) => c.put(e.request, copy));
-      return r;
-    }).catch(() => caches.match(e.request).then((r) => r || caches.match("./")))
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
   );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
